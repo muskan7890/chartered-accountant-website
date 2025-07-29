aeb1108efdc508799d65aa9135458cae2f7ef5ef
Kranti Kumar <kkranti1998@gmail.com>
2025-07-29 21:24:36 +0530

    Fix issue for Railway deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//   experimental: {
+//     serverComponentsExternalPackages: [],
+//   },
+//   trailingSlash: false,
+//   swcMinify: true,
+
+//   output: 'standalone'
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -9,13 +31,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
+  // Minimal config to avoid routes manifest issues
   trailingSlash: false,
   swcMinify: true,
-
-  output: 'standalone'
 }
 
 module.exports = nextConfig
